a29c67d7c972a9e693bd7fa1d72f12acf2fbbd70
Juan Ignacio <juan.ignacio848@gmail.com>
2020-09-10 20:33:44 -0300

    Initial layout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="app">
+      <div className="app__top">
+        <img className="app__logo"
+             alt=""
+             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"/>
+        <h1>Reels</h1>
+      </div>
+
+      <div className="app__videos">
+        {/*container of app__videos (scrollable container)*/} 
+        {/*<Video />*/} 
+        {/*<Video />*/} 
+        {/*<Video />*/} 
+      </div>
     </div>
   );
 }
